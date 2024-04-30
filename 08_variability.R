2155d656749c0e574ce4c5d92e207c2d97da71ff
KliziaS <162352531+KliziaS@users.noreply.github.com>
2024-04-30 17:43:10 +0200

    Update 08_variability.R

--- 08_variability.R
+++ 08_variability.R
@@ -73,3 +73,60 @@ tre malattie di daltonismo
 install.packages("viridis")
 library(viridis)
 
+
+se faccio un plot
+                  l'acqua viene semore nera perche in querlla banda l'acu assorbe tutto,a che l'infrarosso e diventa nera
+
+ancora la funzione 
+colorRampPalette()
+viridisc <- colorRampPalette(viridis(7))(100)
+viridisc <- colorRampPalette(plasma(7))(256)
+plot(sd3, col=viridisc)
+# img
+
+
+
+# exercise: calculate the sd mw of 7 pixels
+sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
+plot(sd7, col=viridisc)
+# img blu
+
+13^2=169
+sd13 <- focal(nir, matrix(1/169, 13, 13), fun=sd)
+plot(sd13, col=viridisc)
+
+sdstack <- c(sd3, sd7, sd13)
+plot(sdstack, col=viridisc)
+
+quale è la migliore? nessuna
+dipende dallos copo
+es voglio vedere la media della viomadda allora dovrei suare una piuù ampia
+solitmanete in tutti i tipi di analisi che vengono fatti spazialm x individuare la variabilita o le caratt a liv statistico si usano
+
+okular Resolution Problems for
+risoluzione piu grossolana: le forme diventano tutte uguali
+non ce una finestra migliore dell altra, dipende solo dallo scopo e dalla scala
+
+
+altro metodo: analisi multivariabile
+spiegare la variabilità, la varianza (si dice nel linguaggio modellistico)
+giochino statistico: passo un nuovo asse nel range + ampio e lo chiamo componente principale
+stressi dati ma ho passato un nuovo sistema di assi, il rpimo spiega la maggior parte, il secondo una piccola
+al pisto di scegliere il nir prendo queso asse, perdero il 10% dell'informazionema non fa niente
+
+# x vedere correlazione fra le avrie bande
+pairs(sent) 
+passo da una img da n bande che +o- hanno le stesse info 50 e 50%
+ad un nuovo asse che invece possiede un range molto piu ampio 90% e 10%
+
+                  prox lezione rapporto tra salvator dali e analisi multivariab
+                  sviluppo di dit multidimensionali tra dali e baracioff
+                  # multivariate analysis
+                  invece di scegliere il nir scelgo la componente
+                  prendo un sacco di dati multivaraib e li compatto in queso
+                  metodo: analisi dei componentit principali (PC)
+
+
+
+
+
