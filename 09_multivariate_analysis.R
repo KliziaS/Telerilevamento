9202103747d9220f52ac61652501a39ade68a295
KliziaS <162352531+KliziaS@users.noreply.github.com>
2024-05-02 17:49:20 +0200

    Update 09_multivariate_analysis.R

--- 09_multivariate_analysis.R
+++ 09_multivariate_analysis.R
@@ -41,11 +41,41 @@ causalita
 se uso delle variabili attenzione xk non e detto he una sia la causa dell'altra
 quando correlo variabili tra loro non x forza c'è una causalità tra le due nonostante sono correlate
 non sempre qdue variabili correlate sono anche causalmente correlate.
+se non c'è correlazione origi tra i dati allora neanche pi ha un senso
 
+fuznione 
+causality: causal conclusions from observational data
 
+# PCA(principal component analysis)
+im.pca(sentdo)
+pcimage <- im.pca(sentdo)
 
+Standard deviations (1, .., p=4):
+[1] 1336.4568  543.8798   43.7861   36.9835
 
+cio che esce cos'è?
+esce il range di ogni component eprincipale, quindi 4 componenti dprinc anche se nel plto did default nel plotto 3
 
+voglio estarrre la 5 di ogni componente, quindi prima di ttuto mis erve la somma
+sum(1336.4568  543.8798   43.7861   36.9835)
+tot <- sum(1336.4568,  543.8798,   43.7861,   36.9835)
+1961.1062
 
+1336.4568*100/tot
+543.8798*100/tot
+43.7861*100/tot
+36.9835*100/tot
 
+> 1336.4568*100/tot = 68.14811
+> 543.8798*100/tot = 27.73332
+> 43.7861*100/tot = 2.232725
+> 36.9835*100/tot = 1.885849
 
+vir <- colorRampPalette(viridis(7))(100)
+plot(pcimage, col=vir)
+plot(pcimage, col=viridis(100))
+
+usando il pacchetto viridis 
+plot(pcimage, col=plasma(100))
+
+scrivere una tesi x lunedi alle 9 o martedi lateX
